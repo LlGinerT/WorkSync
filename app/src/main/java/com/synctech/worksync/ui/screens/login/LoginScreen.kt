8b66af9a0456a6e7028bde35d2246391f89596ff
LlGinerT <lginertendero@gmail.com>
2025-03-21 21:54:43 +0100

    Refactorizado LoginScreen, creado componente AuthInputField

--- app/src/main/java/com/synctech/worksync/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/synctech/worksync/ui/screens/login/LoginScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.synctech.worksync.ui.components.AuthInputField
 import com.synctech.worksync.ui.session.SessionViewModel
 
 /**
@@ -39,44 +40,24 @@ fun LoginScreen(
         if (state.isLoading) {
             CircularProgressIndicator()
         } else {
-            OutlinedTextField(
+            AuthInputField(
                 value = state.email,
                 onValueChange = loginViewModel::onEmailChanged,
-                label = { Text("Email") },
-                isError = state.emailError != null,
-                supportingText = {
-                    state.emailError?.let {
-                        Text(
-                            it,
-                            color = MaterialTheme.colorScheme.error
-                        )
-                    }
-                },
-                colors = outlineColors(isError = state.emailError != null),
-                modifier = Modifier.fillMaxWidth()
+                label = "Email",
+                errorMessage = state.emailError
             )
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            OutlinedTextField(
+            AuthInputField(
                 value = state.password,
+                // esta sintaxis en onValueChange es un function reference, le pasa una función como
+                // parametro a otro componente
+                // seria lo mismo que = {loginViewModel.onPasswordChanged(it)} pero mas corto.
                 onValueChange = loginViewModel::onPasswordChanged,
-                label = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                isError = state.passwordError != null,
-                supportingText = {
-                    state.passwordError?.let {
-                        // ?.let es una forma de decir si no es null haz algo,
-                        // nos ahorra el if (state.blabla != null) por complicarlo un poco
-                        // y aprender cosas nuevas
-                        Text(
-                            it,
-                            color = MaterialTheme.colorScheme.error
-                        )
-                    }
-                },
-                colors = outlineColors(isError = state.passwordError != null),
-                modifier = Modifier.fillMaxWidth()
+                label = "Contraseña",
+                errorMessage = state.passwordError,
+                isPassword = true
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -91,16 +72,3 @@ fun LoginScreen(
         }
     }
 }
-
-/**
- * Función auxiliar para manejar el color del `OutlinedTextField`
- *
- * @param isError Boolean que comprueba si en el estado existe un `emailError` o un `passwordError`
- */
-@Composable
-fun outlineColors(isError: Boolean) =
-    OutlinedTextFieldDefaults.colors(
-        focusedBorderColor = if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.outlineVariant,
-        unfocusedBorderColor = if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.outline
-        //TODO(Comprobar los colores del outline en figma)
-    )
