e8b2fa1136187676d981c21d67d2287fc1acb5ac
lbr90 <lbr90@hotmail.com>
2025-03-22 09:59:51 +0100

    Nueva clase WorkViewModelFactory.kt para manejar el viewmodel. Todavía hay que manejar si hay trabajos o no y el adminstrador.

--- app/src/main/java/com/synctech/worksync/data/repository/FakeWorkDataRepository.kt
+++ app/src/main/java/com/synctech/worksync/data/repository/FakeWorkDataRepository.kt
@@ -14,9 +14,27 @@ class FakeWorkDataRepository:WorkRepository{
         Work(
             jobName = "Instalación Router",
             clientName = "Juan Pérez",
-            description = "Instalación de red eléctrica en oficina",
+            description = "Instalación de Router en el Salón",
             address = "Gran Vía"
-        )
+        ),
+        Work(
+            jobName = "Configuración de Red Wifi",
+            clientName = "Elisa Main",
+            description = "Asignación de un nombre único para la red (SSID) ",
+            address = "Sol"
+    ),
+        Work(
+            jobName = "Optimización de la Señal Red Wifi",
+            clientName = "Daniel Rodriquez",
+            description = "Mejorar la cobertura y el rendimiento de una red Wi-Fi.Mover el router a una ubicación más central ",
+            address = "Chamberí"
+    ),
+        Work(
+            jobName = "Configuración de Red Local",
+            clientName = "Sara Ban",
+            description = "Configuración red local que conecta varios dispositivos mediante cables Ethernet ",
+            address = "Malasaña"
+    )
     )
         override fun getWork(): List<Work> = mockWorkData
 }
